6b094b038a7ed3e5bb9f72eed599e8f17854cb8b
GBTC406010UR <GBTC406010UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-24 12:08:12 -0400

    initial commit

---
+++ src/main/java/com/example/demo/Homecontroller.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class Homecontroller {
+    @Autowired
+    TodoRepository todoRepository;
+
+
+    @RequestMapping("/")
+    public String todolist(Model model){
+        model.addAttribute("todo", todoRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+    public String todoform(Model model){
+        model.addAttribute("todo", new Todo());
+        return "todoform";
+
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Todo todo, BindingResult result){
+        if (result.hasErrors()) {
+            return "todoform";
+
+        }
+        todoRepository.save(todo);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showTodo(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "show";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateTodo(@PathVariable("id") Long id, Model model)
+    {
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "todoform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delTodo(@PathVariable("id") Long id){
+        todoRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+}
+
+
---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,85 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 4)
+    private String title;
+
+    @NotNull
+    @Size(max = 150)
+    private String content;
+
+    @NotNull
+    @Size(min = 4)
+    private String taskname;
+
+    @NotNull
+    @Size(min = 5)
+    private String duedate;
+
+    @NotNull
+    @Size(min = 2)
+    private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskname() {
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+    public String getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(String duedate) {
+        this.duedate = duedate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
+
